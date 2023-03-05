57747b8c3265195cc6c91d3bc2060eb8128ea942
Alexey Lapshev <alapshev@emphasoft.com>
2023-03-05 21:53:05 +0400

    db string

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -19,7 +19,7 @@ class User(db.Model):
     __table_args__ = (CheckConstraint("balance >= 0.00"),)
 
     id = db.Column(db.Integer(), primary_key=True)
-    name = db.Column(db.Unicode(), nullable=False)
+    name = db.Column(db.String(50), nullable=False)
     balance = db.Column(db.DECIMAL(10, 2), default=0.00, nullable=False)
 
 
